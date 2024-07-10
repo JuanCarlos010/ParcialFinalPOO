625c5373b54486ac4c3bac3b6ace57919d0bae78
GabrielleZeti <69438737+GabrielleZeti@users.noreply.github.com>
2024-07-09 22:40:39 -0600

    Update HelloApplication.java

--- Banco/src/main/java/com/example/banco/HelloApplication.java
+++ Banco/src/main/java/com/example/banco/HelloApplication.java
@@ -7,17 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application { // 00056122 define la clase principal de la aplicacion
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cliente-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 890, 500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException { // 00056122 inicia la aplicacion JavaFX
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cliente-view.fxml")); // 00056122 carga el archivo FXML
+        Scene scene = new Scene(fxmlLoader.load(), 890, 500); // 00056122 crea una nueva escena
+        stage.setTitle("Hello!"); // 00056122 establece el titulo de la ventana
+        stage.setScene(scene); // 00056122 asigna la escena a la ventana
+        stage.show(); // 00056122 muestra la ventana
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) { // 00056122 metodo principal que lanza la aplicacion
+        launch(); // 00056122 lanza la aplicacion JavaFX
     }
 }
